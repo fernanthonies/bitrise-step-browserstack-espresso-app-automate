90411053188f77aa5633e38981ebd162b35ce89c
Anthony Fernandes <anthonyf@phase2online.com>
2019-01-21 17:53:32 -0600

    added remaining options to script and step file

--- step.sh
+++ step.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#set -ex
+set -ex
 echo "uploading app apk to browserstack"
 upload_app_response="$(curl -u $browserstack_username:$browserstack_access_key -X POST https://api-cloud.browserstack.com/app-automate/upload -F file=@$app_apk_path)"
 app_url=$(echo "$upload_app_response" | jq .app_url)
@@ -13,7 +13,19 @@ json=$( jq -n \
                 --argjson app_url $app_url \
                 --argjson test_url $test_url \
                 --argjson devices ["$browserstack_device_list"] \
-                '{devices: $devices, app: $app_url, testSuite: $test_url}')
+                --argjson package ["$browserstack_package"] \
+                --argjson class ["$browserstack_class"] \
+                --argjson annotation ["$browserstack_annotation"] \
+                --arg size "$browserstack_size" \
+                --arg logs "$browserstack_device_logs" \
+                --arg video "$browserstack_video" \
+                --arg loc "$browserstack_local" \
+                --arg locId "$browserstack_local_identifier" \
+                --arg gpsLocation "$browserstack_gps_location" \
+                --arg language "$browserstack_language" \
+                --arg locale "$browserstack_locale" \
+                --arg callback "$callback_url" \
+                '{devices: $devices, app: $app_url, testSuite: $test_url, package: $package, class: $class, annotation: $annotation, size: $size, logs: $logs, video: $video, local: $loc, localIdentifier: $locId, gpsLocation: $gpsLocation, language: $language, locale: $locale, callbackURL: $callback}')
 run_test_response="$(curl -X POST https://api-cloud.browserstack.com/app-automate/espresso/build -d \ "$json" -H "Content-Type: application/json" -u "$browserstack_username:$browserstack_access_key")"
 build_id=$(echo "$run_test_response" | jq .build_id | envman add --key BROWSERSTACK_BUILD_ID)
 
